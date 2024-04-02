bc0e529205a76d56f02f2ddc20d25a9237506e7e
pleasantDream <3385369312@qq.com>
2024-04-02 11:43:43 +0800

    后台环境搭建

---
+++ background/src/main/java/org/example/backgroundApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class backgroundApplication
+{
+    public static void main( String[] args ){
+        SpringApplication.run(backgroundApplication.class, args);
+    }
+}
