99c2b9b7f004c928c05304ad2c3f8f7c6c2a3a5d
pleasantDream <3385369312@qq.com>
2024-04-14 00:05:10 +0800

    vscode测试push代码到github

--- foreground/src/main.js
+++ foreground/src/main.js
@@ -1,13 +1,13 @@
 import './assets/main.scss'
-
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
 import App from './App.vue'
 import router from './router/index.js'
+
 import { createPinia } from 'pinia'
 import { createPersistedState } from 'pinia-persistedstate-plugin'
+
 const pinia = createPinia()
 const persist = createPersistedState()
 pinia.use(persist)
